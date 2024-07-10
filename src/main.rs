bee7838764784a1095a3345ac4186d2a728848a1
perfectbogus <war_army_10@hotmail.com>
2024-07-10 09:55:17 -0600

    Program Flow Control: Finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::cmp::max;
+use std::i32::MAX;
+
 fn main() {
     let mut x = 10;
     println!("x is {}", x);
@@ -401,6 +404,39 @@ fn main() {
         }
         println!();
     }
+
+    println!();
+    println!("-- Challenge Program Flow Control ---");
+    let numbers = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
+    let mut max: i32 = i32::MIN;
+    let mut min: i32 = i32::MAX;
+    let mut mean: f64 = 0.0;
+
+    for &num in numbers.iter() {
+        if num > max {
+           max = num;
+        }
+
+        if num < min {
+            min = num;
+        }
+
+        mean += num as f64;
+    }
+
+    mean /= numbers.len() as f64;
+
+    println!("Max: {}, Min: {}, Mean: {}", max, min, mean);
+    assert_eq!(max, 56);
+    assert_eq!(min, -18);
+    assert_eq!(mean, 12.5);
+    println!("Test passed");
+
+
+
+
+
+
 
 
 }
