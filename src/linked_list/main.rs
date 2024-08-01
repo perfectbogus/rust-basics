6164eca6a0179bd577309dc9aab7862891ed4769
eduardo escobar <war_army_10@hotmail.com>
2024-07-31 22:13:48 -0600

    trying linked list

--- src/linked_list/main.rs
+++ src/linked_list/main.rs
@@ -1,4 +1,20 @@
+struct Node {
+    val: i32,
+    next: Option<Node>
+}
 
+impl Node {
+    fn new(val: i32) -> Node {
+        Node {
+            val,
+            next: None,
+        }
+    }
+}
+
+struct LinkedList {
+    head: Node
+}
 fn main() {
     let x = Box::new(5);
 
