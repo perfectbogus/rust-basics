c7670a1731664329c9ad177ee3c45778cc2d2cfa
eduardo escobar <war_army_10@hotmail.com>
2024-08-03 00:53:26 -0600

    finish singly linked

--- src/ok_singly_linked.rs
+++ src/ok_singly_linked.rs
@@ -16,6 +16,10 @@ pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>
 }
 
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>
+}
+
 impl<T> List<T> {
 
     pub fn iter(&self) -> Iter<T> {
@@ -53,6 +57,21 @@ impl<T> List<T> {
 
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut { next: self.head.as_deref_mut() }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
             &mut node.elem
         })
     }
@@ -99,9 +118,9 @@ mod test {
         list.push(3);
 
         let mut iter = list.into_iter();
-        assert_eq!(iter.next(), Some(&3));
-        assert_eq!(iter.next(), Some(&2));
-        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
     }
 
     #[test]
