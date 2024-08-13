6f2e2adf47fc76f1adbc80c33a6f50f6a2ee0971
eduardo escobar <war_army_10@hotmail.com>
2024-08-12 22:07:45 -0600

    add memory example

---
+++ src/design_patterns/idioms/2_7_mem/main.rs
@@ -0,0 +1,15 @@
+use std::mem;
+
+enum MyEnum {
+    A {name: String, x: u8 },
+    B {name: String}
+}
+
+fn a_to_b(e: &mut MyEnum) {
+    if let MyEnum::A { name , x: 0} = e {
+
+    }
+}
+fn main() {
+
+}
