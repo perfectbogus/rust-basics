19eb80daf9dfb60cd148da7919f67de0fe869735
eduardo escobar <war_army_10@hotmail.com>
2024-10-11 23:18:20 -0600

    smart pointer project

---
+++ smart_pointers/src/main.rs
@@ -0,0 +1,57 @@
+use std::ops::Deref;
+
+fn main() {
+    let b = Box::new(5);
+    println!("Hello, world! {b}");
+
+    header("Following the Pointer to the Value");
+    let x = 5;
+    let y = &x;
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    println!("X: {}, Y: {}", x, *y);
+
+    header("Using Box<t> like a reference");
+    let x2 = 5;
+    let y2 = Box::new(x2);
+    println!("X: {}, Y: {}", x2, *y2);
+
+    header("Defining our own Smart Pointer");
+    let x3 = 5;
+    let y3 = MyBox(x3);
+    assert_eq!(5, x3);
+    assert_eq!(5, *y3);
+    println!("X: {}, Y: {}", x3, *y3);
+
+    header("Implicit defer coercions with functions and methods");
+    hello("Rust");
+    let m = MyBox(String::from("Rust"));
+    hello(&m);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}");
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn header(message: &str) {
+    println!();
+    println!("#############################");
+    println!("### {message} ###");
+    println!("#############################")
+}
