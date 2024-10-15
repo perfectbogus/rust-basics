bab9d6a5dd3efe1e2494c92c8e6a6aecc03b4ae1
eduardo escobar <war_army_10@hotmail.com>
2024-10-14 21:50:28 -0600

    cycle example

--- cycle_memory_leak/src/main.rs
+++ cycle_memory_leak/src/main.rs
@@ -1,8 +1,24 @@
 use std::cell::RefCell;
 use std::rc::Rc;
+use crate::List::{Cons, Nil};
 
 fn main() {
-    println!("Hello, world!");
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
 }
 
 #[derive(Debug)]
@@ -14,8 +30,8 @@ enum List {
 impl List {
     fn tail (&self) -> Option<&RefCell<Rc<List>>> {
         match self {
-            List::Cons(_, item) => Some(item),
-            List::Nil => None,
+            Cons(_, item) => Some(item),
+            Nil => None,
         }
     }
 }
