cf318008a11a1dcd36683dc88f3918223f046398
eduardo escobar <war_army_10@hotmail.com>
2024-10-25 23:54:53 -0600

    Continue add functions on system invetory game

--- challenges/system_inventory_store/src/main.rs
+++ challenges/system_inventory_store/src/main.rs
@@ -8,6 +8,28 @@ struct Game {
     stock: u32,
 }
 
+struct GameStore {
+    inventory: HashMap<String, Game>,
+}
+
+impl Game {
+    fn new(title: String, genre: String, price: f64) -> Self {
+        Game {title, genre, price, stock: 0}
+    }
+}
+
+impl GameStore {
+    fn new() -> Self {
+        Self { inventory: HashMap::new() }
+    }
+
+    //Insert using different methods
+    fn add_game_simple(&mut self, game: Game) -> Option<Game> {
+        //TODO: Use simple insert method
+        unimplemented!()
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
