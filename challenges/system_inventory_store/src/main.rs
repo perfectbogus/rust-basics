069dfa4470fa5e168dc89eaa104916b6256f302a
eduardo escobar <war_army_10@hotmail.com>
2024-10-25 23:49:17 -0600

    inventory system store

---
+++ challenges/system_inventory_store/src/main.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+struct Game {
+    title: String,
+    genre: String,
+    price: f64,
+    stock: u32,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
