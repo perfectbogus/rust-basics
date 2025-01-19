dce5a329d391e6d64823ede889bc3657cfb8f35e
eduardo escobar <war_army_10@hotmail.com>
2025-01-18 20:55:22 -0600

    solving functions

--- challenges/library_management_system_vector/src/main.rs
+++ challenges/library_management_system_vector/src/main.rs
@@ -37,7 +37,46 @@ impl Library {
         }
     }
 
+    fn remove_book_by_id(&mut self, id: u32) -> Option<Book> {
+        for (i, book) in self.books.iter().enumerate() {
+            if book.id == id {
+                Some(self.books.remove(i));
+            }
+        }
+        None
+    }
 
+    fn remove_last(&mut self) -> Option<Book> {
+        self.books.pop()
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_test_book(id: u32) -> Book {
+        Book {
+            id,
+            title: format!("Books {}", id),
+            category: String::from("Test")
+        }
+    }
+
+    #[test]
+    fn test_add_and_remove() {
+        let mut library = Library::new();
+
+        // Test adding
+        library.add_book(create_test_book(1));
+        assert_eq!(library.books.len(), 1);
+
+        // Test removing
+        let removed = library.remove_book_by_id(1);
+        assert!(removed.is_some());
+        assert_eq!(library.books.len(), 0);
+    }
 }
 
 
